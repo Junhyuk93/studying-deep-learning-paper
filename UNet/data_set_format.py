72c0ee2ddd71fa2cf4f6e2247954f2a3ba73768b
ParkJunHyuk <akznsk1@gmail.com>
2022-02-23 22:13:18 +0900

    update Unet

---
+++ UNet/data_set_format.py
@@ -0,0 +1,92 @@
+# 패키지 등록
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# data load
+
+dir_data = './datasets'
+
+name_label = 'train-labels.tif'
+name_input = 'train_volume.tif'
+
+# img_label = Image.open(os.path.join(dir_data, name_label))
+img_label = Image.open('C:/Users/akzns/Desktop/폴더/ai/논문/PyTorchImplementation/UNet/datasets/train-labels.tif')
+# img_input = Image.open(os.path.join(dir_data, name_input))
+img_input = Image.open("C:/Users/akzns/Desktop/폴더/ai/논문/PyTorchImplementation/UNet/datasets/train-volume.tif")
+
+
+ny, nx = img_label.size
+nframe = img_label.n_frames
+
+# train,val,test
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+
+#
+id_frame = np.arange(nframe)
+np.random.shuffle(id_frame)
+
+# dataset save
+offset_nframe = 0
+
+for i in range(nframe_train):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i),label_)
+    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i),input_)
+
+# valset
+offset_nframe += nframe_train
+
+for i in range(nframe_val):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i),label_)
+    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i),input_)
+
+# testset
+offset_nframe += nframe_val
+
+for i in range(nframe_test):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i),label_)
+    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i),input_)
+
+
+plt.subplot(121)
+plt.imshow(label_, cmap='gray')
+plt.title('label')
+
+plt.subplot(122)
+plt.imshow(input_, cmap='gray')
+plt.title('input')
+
+plt.show()

c43ccb76092ab5929484932d3861a8ea51c36fed
Junhyuk93 <akznsk1@gmail.com>
2022-03-15 23:00:08 +0900

    update GAN code ver2

--- DCGAN/model.py
+++ DCGAN/model.py
@@ -1,4 +1,5 @@
 import os
+from turtle import forward
 import numpy as np
 from sklearn.preprocessing import KernelCenterer
 
@@ -15,8 +16,63 @@ class DCGAN(nn.Module):
     def __init__(self, in_channels, out_channels, nker=64, norm='bnorm'):
         super(DCGAN, self).__init__()
 
-        self.dec1 = DECBR2d(1 * in_channels, 8 *nker, Kernel_size=4, stride=1,
+        self.dec1 = DECBR2d(1 * in_channels, 8 * nker, Kernel_size=4, stride=1,
                             padding=0, norm=norm, relu=0.0, biase=False)
+
+        self.dec2 = DECBR2d(8 * nker, 4 * nker, kernel_size=4, stride=2,
+                            padding=1, norm=norm, relu=0.0, bias=False)
+
+        self.dec3 = DECBR2d(4 * nker, 2 * nker, kernel_size=4, stride=2,
+                            padding=1, norm=norm, relu=0.0, bias=False)
+
+        self.dec4 = DECBR2d(2 * nker, 1 * nker, kernel_size=4, stride=2,
+                            padding=1, norm=norm, relu=0.0, bias=False)
+
+        self.dec5 = DECBR2d(1 * nker, out_channels, kernel_size=4, stride=2,
+                            padding=1, norm=None, relu=None, bias=False) # tanh func 사용
+
+    def forward(self, x):
+
+        x = self.dec1(x)
+        x = self.dec2(x)
+        x = self.dec3(x)
+        x = self.dec4(x)
+        x = self.dec5(x)
+
+        x = torch.tanh(x)
+
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, out_channels, nker=64, norm='bnorm'):
+        super(Discriminator, self).__init__()
+
+        self.enc1 = CBR2d(1 * in_channels, 1 * nker, kernel_size=4, stride=2,
+                        padding=1, norm=norm, relu=0.2, bias=False)
+
+        self.enc2 = CBR2d(1 * nker, 2 * nker, kernel_size=4, stride=2,
+                        padding=1, norm=norm, relu=0.2, bias=False)
+
+        self.enc3 = CBR2d(2 * nker, 4 * nker, kernel_size=4, stride=2,
+                        padding=1, norm=norm, relu=0.2, bias=False)
+
+        self.enc4 = CBR2d(4 * nker, 8 * nker, kernel_size=4, stride=2,
+                        padding=1, norm=norm, relu=0.2, bias=False)
+
+        self.enc5 = CBR2d(1 * nker, out_channels, kernel_size=4, stride=2,
+                        padding=1, norm=None, relu=None, bias=False)
+                
+    def forward(self,x):
+
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
+        x = self.enc4(x)
+        x = self.enc5(x)
+
+        x = torch.sigmoid(x)
+
+        return x
 
 ## 네트워크 구축하기
 # U-Net: Convolutional Networks for Biomedical Image Segmentation
